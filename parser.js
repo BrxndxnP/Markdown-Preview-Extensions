9662516faf0b67a1d9ed4a9e3fc5b033373a1f29
BrxndxnP <brendan.phom@gmail.com>
2021-04-16 17:03:40 -0400

    Added elements to parser.js

--- parser.js
+++ parser.js
@@ -2,26 +2,65 @@ module.exports = {
   onWillParseMarkdown: function (markdown) {
     return new Promise((resolve, reject) => {
 
+      /**
+       * Import js files
+       */
       var urls = [
         "https://raw.githubusercontent.com/BrxndxnP/Markdown-Preview-Extensions/main/scripts/toc.js",
         "https://raw.githubusercontent.com/BrxndxnP/Markdown-Preview-Extensions/main/scripts/codeblock.js",
-        "https://raw.githubusercontent.com/BrxndxnP/Markdown-Preview-Extensions/main/scripts/table-cell-visibility.js",
+        // "https://raw.githubusercontent.com/BrxndxnP/Markdown-Preview-Extensions/main/scripts/table-cell-visibility.js",
       ];
 
       for (let i = 0; i < urls.length; i++) {
         const path = urls[i];
-        markdown = markdown + `
-
-@import "${path}" {cmd hide}
-
-        `;
+        markdown = markdown + `\r\n\r\n@import "${path}" {cmd hide}\r\n\r\n`;
       }
+
+
+     /**
+      * Allow centering content by using a comment tags
+      * 
+      * Example: 
+      *    <!-- <center> -->
+      * 
+      *    markdown content
+      * 
+      *    <!-- </center> -->
+      */
+      markdown = markdown.replace(/<\!--[ \t]+<center>+[ \t]+-->/gm, ($0) => '\r\n\r\n<div class="block-center">\r\n\r\n');
+      markdown = markdown.replace(/<\!--[ \t]+<\/center>+[ \t]+-->/gm, ($0) => '\r\n\r\n</div>\r\n\r\n');
+
+      /**
+       * Allow aligning content side by side by using a comment tags
+       * 
+       * Example: 
+       * 
+       *    <!-- <horizontal> -->
+       *      <!-- <horizontal-part> -->
+       * 
+       *        Content on the left side
+       * 
+       *      <!-- </horizontal-part> -->
+       *      <!-- <horizontal-part> -->
+       * 
+       *        Content on the right side
+       * 
+       *      <!-- </horizontal-part> -->
+       *    <!-- <horizontal> -->
+       */
+      markdown = markdown.replace(/<\!--[ \t]+<horizontal>+[ \t]+-->/gm, ($0) => '\r\n\r\n<div class="block-horizontal">\r\n\r\n');
+      markdown = markdown.replace(/<\!--[ \t]+<\/horizontal>+[ \t]+-->/gm, ($0) => '\r\n\r\n</div>\r\n\r\n<div style="clear:both;"><div>\r\n\r\n');
+      markdown = markdown.replace(/<\!--[ \t]+<horizontal\-part>+[ \t]+-->/gm, ($0) => '\r\n\r\n<div class="block-horizontal-part">\r\n\r\n');
+      markdown = markdown.replace(/<\!--[ \t]+<\/horizontal\-part>+[ \t]+-->/gm, ($0) => '\r\n\r\n</div>\r\n\r\n');
 
       return resolve(markdown);
     })
   },
   onDidParseMarkdown: function (html, { cheerio }) {
     return new Promise((resolve, reject) => {
+
+      // Remove '-' from table tags in the preview
+      html = html.replace(/<td>-<\/td>/gm, ($0) => $0.replace('-', ''));
       return resolve(html)
     })
   },
